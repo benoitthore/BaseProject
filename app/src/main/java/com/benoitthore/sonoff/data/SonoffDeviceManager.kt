af33f46a2c1a3433e1d94efb072dbefdbcb7c66b
Benoit Thore <thorebenoit@gmail.com>
2020-08-08 02:59:51 +0100

    preparing hostnames

--- app/src/main/java/com/benoitthore/sonoff/data/SonoffDeviceManager.kt
+++ app/src/main/java/com/benoitthore/sonoff/data/SonoffDeviceManager.kt
@@ -1,6 +1,7 @@
 package com.benoitthore.sonoff.data
 
 import com.benoitthore.base.lib.data.*
+import java.net.InetAddress
 
 interface SonoffDeviceManager {
 
@@ -8,6 +9,9 @@ interface SonoffDeviceManager {
 
     suspend fun toggle(): ApiResponse<PowerData>
     suspend fun turn(mode: Boolean): ApiResponse<PowerData>
+
+    suspend fun setHostName(hostName: String): ApiResponse<HostNameData>
+    suspend fun getHostName(): ApiResponse<HostNameData>
 
     suspend fun turnOff() = turn(false)
     suspend fun turnOn() = turn(true)
@@ -20,16 +24,25 @@ class SonoffServiceBuilder : (SonoffDeviceId) -> SonoffService {
 
 class SonoffDeviceManagerBuilder(
         private val serviceBuilder: SonoffServiceBuilder,
-        private val dtoMapper: Mapper<PowerResponseDTO, PowerData>
+        private val powerResponseMapper: Mapper<PowerResponseDTO, PowerData>,
+        private val hostNameResponseMapper: Mapper<HostNameResponseDTO, HostNameData>
 ) : (SonoffDeviceId) -> SonoffDeviceManager {
-    override fun invoke(id: SonoffDeviceId): SonoffDeviceManager = SonoffDeviceManagerImpl(serviceBuilder(id), dtoMapper)
+    override fun invoke(id: SonoffDeviceId): SonoffDeviceManager = SonoffDeviceManagerImpl(serviceBuilder(id), powerResponseMapper, hostNameResponseMapper)
 
 }
 
-private class SonoffDeviceManagerImpl(val service: SonoffService, val dtoMapper: Mapper<PowerResponseDTO, PowerData>) : SonoffDeviceManager {
-    override suspend fun getState(): ApiResponse<PowerData> = service.getPowerState().toApiResponse(dtoMapper)
-    override suspend fun toggle(): ApiResponse<PowerData> = service.toggle().toApiResponse(dtoMapper)
-    override suspend fun turn(mode: Boolean): ApiResponse<PowerData> = if (mode) service.setOn().toApiResponse(dtoMapper) else service.setOff().toApiResponse(dtoMapper)
+private class SonoffDeviceManagerImpl(
+        val service: SonoffService,
+        val powerResponseMapper: Mapper<PowerResponseDTO, PowerData>,
+        val hostNameResponseMapper: Mapper<HostNameResponseDTO, HostNameData>
+
+) : SonoffDeviceManager {
+
+    override suspend fun getState(): ApiResponse<PowerData> = service.getPowerState().toApiResponse(powerResponseMapper)
+    override suspend fun toggle(): ApiResponse<PowerData> = service.toggle().toApiResponse(powerResponseMapper)
+    override suspend fun turn(mode: Boolean): ApiResponse<PowerData> = if (mode) service.setOn().toApiResponse(powerResponseMapper) else service.setOff().toApiResponse(powerResponseMapper)
+    override suspend fun setHostName(hostName: String): ApiResponse<HostNameData> = service.setHostName(hostName).toApiResponse(hostNameResponseMapper)
+    override suspend fun getHostName(): ApiResponse<HostNameData> = service.getHostName().toApiResponse(hostNameResponseMapper)
 }
 
 interface SonoffRepository {
@@ -40,7 +53,13 @@ interface SonoffRepository {
 }
 
 // TODO Inject
-class SonoffRepositoryImpl(private val deviceManagerBuilder: (SonoffDeviceId) -> SonoffDeviceManager = SonoffDeviceManagerBuilder(SonoffServiceBuilder(), PowerResponseMapper)) : SonoffRepository {
+// TODO handle get/set hostname
+// TODO Scan network for hosts and return a map of HostNames to SonoffDeviceId (IP)
+class SonoffRepositoryImpl(
+        private val deviceManagerBuilder: (SonoffDeviceId) -> SonoffDeviceManager
+        = SonoffDeviceManagerBuilder(SonoffServiceBuilder(), PowerResponseMapper, HostNameResponseMapper)
+) : SonoffRepository {
+
 
     private val map = mutableMapOf<SonoffDeviceId, SonoffDeviceManager>()
 
@@ -53,11 +72,17 @@ class SonoffRepositoryImpl(private val deviceManagerBuilder: (SonoffDeviceId) ->
 
     override suspend fun addDeviceManager(id: SonoffDeviceId): SonoffDeviceManager? =
             getDeviceManager(id)?.also { deviceManager ->
-                        map[id] = deviceManager
-                    }
+                map[id] = deviceManager
+            }
 
     override fun removeDevice(id: SonoffDeviceId): Boolean = map.remove(id) != null
 
     private suspend fun SonoffDeviceManager.isReachable(): Boolean = getState() !is ApiResponse.Success
 
+}
+
+fun main() {
+    println(
+            InetAddress.getByName("Sonoff1")
+    )
 }
--- app/src/main/java/com/benoitthore/sonoff/data/SonoffService.kt
+++ app/src/main/java/com/benoitthore/sonoff/data/SonoffService.kt
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import okhttp3.ResponseBody
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface SonoffService {
     @GET("cm?cmnd=Power")
@@ -22,6 +23,13 @@ interface SonoffService {
 
     @GET("cm?cmnd=Power%20Toggle")
     suspend fun toggle(): Response<PowerResponseDTO>
+
+    @GET("cm?cmnd=Hostname%20{name}")
+    suspend fun setHostName(@Path("name") name: String): Response<HostNameResponseDTO>
+
+    @GET("cm?cmnd=Hostname")
+    suspend fun getHostName(): Response<HostNameResponseDTO>
+
 }
 
 data class PowerResponseDTO(@JsonProperty("POWER") val power: String)
@@ -30,6 +38,15 @@ data class PowerData(val isOn: Boolean)
 
 object PowerResponseMapper : Mapper<PowerResponseDTO, PowerData> {
     override fun invoke(dto: PowerResponseDTO) = PowerData(dto.power?.toLowerCase() == "on")
+}
+
+
+data class HostNameResponseDTO(@JsonProperty("Hostname") val hostname: String)
+
+data class HostNameData(val name: String)
+
+object HostNameResponseMapper : Mapper<HostNameResponseDTO, HostNameData> {
+    override fun invoke(dto: HostNameResponseDTO) = HostNameData(dto.hostname)
 }
 
 
